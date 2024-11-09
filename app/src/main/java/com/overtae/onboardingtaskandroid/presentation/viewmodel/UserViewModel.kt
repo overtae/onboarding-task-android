a14b5af3519fa383b444ee18a8abb7b2a34ca6eb
overtae <rlaxodud911@gmail.com>
2024-11-10 04:28:27 +0900

    Feat: 추가된 버튼 디자인에 맞게 기능 구현 후 적용 (로그아웃, 회원탈퇴)

--- app/src/main/java/com/overtae/onboardingtaskandroid/presentation/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/overtae/onboardingtaskandroid/presentation/viewmodel/UserViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.overtae.onboardingtaskandroid.domain.model.Result
 import com.overtae.onboardingtaskandroid.domain.model.User
+import com.overtae.onboardingtaskandroid.domain.usecase.DeleteUserUseCase
 import com.overtae.onboardingtaskandroid.domain.usecase.GetUserUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -14,6 +15,7 @@ import javax.inject.Inject
 @HiltViewModel
 class UserViewModel @Inject constructor(
     private val getUserUseCase: GetUserUseCase,
+    private val deleteUserUseCase: DeleteUserUseCase,
 ) : ViewModel() {
     private val _userResult = MutableStateFlow<Result<User>>(Result.Loading)
     val userResult = _userResult.asStateFlow()
@@ -21,6 +23,12 @@ class UserViewModel @Inject constructor(
     fun getUserByEmail(email: String) {
         viewModelScope.launch {
             _userResult.value = getUserUseCase(email)
+        }
+    }
+
+    fun deleteUser(user: User) {
+        viewModelScope.launch {
+            deleteUserUseCase(user)
         }
     }
 }
